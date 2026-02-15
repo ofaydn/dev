83dff8dcf0806fc3af2c59d789b331460c68308f
Omer Faruk <o.faduzce@gmail.com>
2026-02-15 13:07:39 +0300

    asdf

--- _config.ts
+++ _config.ts
@@ -1,8 +1,8 @@
-import lume from "https://deno.land/x/lume/cli.ts";
-import robots from "lume/plugins/robots.ts";
-import postcss from "lume/plugins/postcss.ts";
-import seo from "lume/plugins/seo.ts";
-import minify_html from "https://deno.land/x/lume/plugins/minify_html.ts";
+import lume from "https://deno.land/x/lume@v3.2.1/mod.ts";
+import robots from "https://deno.land/x/lume@v3.2.1/plugins/robots.ts";
+import postcss from "https://deno.land/x/lume@v3.2.1/plugins/postcss.ts";
+import seo from "https://deno.land/x/lume@v3.2.1/plugins/seo.ts";
+import minify_html from "https://deno.land/x/lume@v3.2.1/plugins/minify_html.ts";
 
 const site = lume();
 
@@ -10,6 +10,8 @@ site.use(robots());
 site.use(postcss());
 site.use(seo());
 site.use(minify_html());
+
 site.copy("style.css");
 site.copy("CNAME");
+
 export default site;
