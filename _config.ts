e5731e73bcc06cc44164f2db50083406aad1212c
Omer Faruk <o.faduzce@gmail.com>
2026-02-15 12:35:02 +0300

    initial repo

---
+++ _config.ts
@@ -0,0 +1,15 @@
+	import lume from "lume/mod.ts";
+import robots from "lume/plugins/robots.ts";
+import postcss from "lume/plugins/postcss.ts";
+import seo from "lume/plugins/seo.ts";
+import minify_html from "lume/plugins/minify_html.ts";
+
+const site = lume();
+
+site.use(robots());
+site.use(postcss());
+site.use(seo());
+site.use(minify_html());
+site.copy("style.css");
+site.copy("CNAME");
+export default site;
