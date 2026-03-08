e35c3cd06e1269a41cfd8b97ee6924b973c87bce
Omer Faruk <o.faduzce@gmail.com>
2026-03-08 21:28:37 +0300

    taslak tammalandı, linkler düznelenecke

--- _config.ts
+++ _config.ts
@@ -1,17 +1,9 @@
-import lume from "https://deno.land/x/lume@v3.2.1/mod.ts";
-import robots from "https://deno.land/x/lume@v3.2.1/plugins/robots.ts";
-import postcss from "https://deno.land/x/lume@v3.2.1/plugins/postcss.ts";
-import seo from "https://deno.land/x/lume@v3.2.1/plugins/seo.ts";
-import minify_html from "https://deno.land/x/lume@v3.2.1/plugins/minify_html.ts";
+import lume from "lume/mod.ts";
+import postcss from "lume/plugins/postcss.ts";
 
 const site = lume();
-
-site.use(robots());
 site.use(postcss());
-site.use(seo());
-site.use(minify_html());
-
-site.copy("style.css");
+site.add("style.css");  // copy static files as-is
 site.copy("CNAME");
 
 export default site;
