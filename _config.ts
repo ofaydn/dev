bd31b92f4ff883247fcaf08f0b00948f375c2147
Omer Faruk <o.faduzce@gmail.com>
2026-03-08 21:53:16 +0300

    site lokasyonu update

--- _config.ts
+++ _config.ts
@@ -1,7 +1,7 @@
 import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
 
-const site = lume();
+const site = lume({ location: new URL("https://ofaydn.github.io/dev/") });
 site.use(postcss());
 site.add("style.css");  // copy static files as-is
 site.copy("CNAME");
